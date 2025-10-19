d3856996986c8532b0526a6b7873f05db4ff68a4
caals666 <192770810+caals666@users.noreply.github.com>
2025-10-19 11:32:25 +0530

    shortened the code and made it simpler

--- script.js
+++ script.js
@@ -2,91 +2,47 @@ const weatherForm = document.querySelector(".weatherForm");
 const cityInput=document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey="YZTAHBJ5WSFEXDCVM4FTZDBKD";
+const btn=document.querySelector("button");
+const err=document.querySelector(".errorDisplay");
+const cityDisplay=document.querySelector(".cityDisplay");
+const tempDisplay=document.querySelector(".tempDisplay");
+const humidityDisplay=document.querySelector(".humidityDisplay");
+const descDisplay=document.querySelector(".descDisplay");
+const weatherEmoji=document.querySelector(".weatherEmoji");
 
 const date=new Date();
 const year=date.getFullYear();
 const month=date.getMonth()+1;
 const day=date.getDate();
 
-weatherForm.addEventListener("submit",async event=>{
-    event.preventDefault();
-    const city = cityInput.value;
-    if(city){
-        try{
-            const weatherData = await getWeatherData(city);
-            displayWeatherInfo(weatherData);
-
-        }
-        catch(error){
-            console.log(error);
-            displayError(error);
-        }
-    }
-    else{
-        displayError("Enter a city");
-    }
-});
-
-function getWeatherData(city){
-    const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${year}-${month}-${day}?key=${apiKey}`;
-    
-    return fetch(apiUrl)
-        .then(response=>{
-            if(!response.ok){
-                throw new Error("Could not fetch weather data");
-            }
-            return response.json();
+function weatherData(){
+    const city=cityInput.value||'mumbai';
+    if(city!==''){
+        cityInput.value="";
+        err.textContent="";
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${year+'-'+month+'-'+day}?key=${apiKey}`)
+        .then(response=>response.json())
+        .then(data=>{
+            displayWeather(data);
+            console.log(data)
         })
-        .catch(error=>displayError(error));
-}
-
-function displayWeatherInfo(data){
-    const {
-    address, 
-    currentConditions: {
-      temp, 
-      humidity, 
-      conditions, 
-      icon
+        .catch(error=>err.textContent=error.message);
     }
-  } = data;
-  
-
-    card.textContent="";
-    card.style.display="flex";
-    
-    const cityDisplay = document.createElement("h1");
-    const tempDisplay = document.createElement("p");
-    const humidityDisplay = document.createElement("p");
-    const descDisplay = document.createElement("p");
-    const weatherEmoji = document.createElement("img");
-
-    cityDisplay.textContent=address.charAt(0).toUpperCase()+address.slice(1).toLowerCase();
-    tempDisplay.textContent=`${((temp-32)/9*5).toFixed(1)}°C`;
-    humidityDisplay.textContent=`Humidity: ${humidity.toFixed(0)}%`;
-    descDisplay.textContent=`${conditions}`;
-    weatherEmoji.src=`https://raw.githubusercontent.com/visualcrossing/WeatherIcons/refs/heads/main/PNG/3rd%20Set%20-%20Color/${icon}.png`;  
-
-    cityDisplay.className="cityDisplay md:text-5xl text-4xl font-bold";
-    tempDisplay.className="md:text-[26px] text-3xl pt-2";
-    humidityDisplay.className="md:text-md font-bold text-2xl pt-2 mb-[25px] italic";
-    descDisplay.className="md:text-md font-bold text-2xl";
-    weatherEmoji.className="md:w-[150px] m-0 w-[100px]";
-    
-    card.appendChild(cityDisplay);
-    card.appendChild(tempDisplay);
-    card.appendChild(humidityDisplay);
-    card.appendChild(descDisplay);
-    card.appendChild(weatherEmoji);
 }
 
-function displayError(message){
-    const errorDisplay=document.createElement("p");
-    errorDisplay.textContent=message;
-    errorDisplay.classList.add("errorDisplay");
-    console.log(message);
-
-    card.textContent = "";
-    card.style.display="flex";
-    card.appendChild(errorDisplay);
+function displayWeather(data){
+    let {address, currentConditions:{temp, humidity, icon,conditions}}=data;
+    temp=((temp-32)*5/9).toFixed(1)+"°C";
+    humidity="Humidity: "+humidity.toFixed()+"%";
+    console.log(address+" "+temp+" "+humidity+" "+icon+" "+conditions);
+    cityDisplay.innerHTML=address;
+    tempDisplay.innerHTML=temp;
+    humidityDisplay.innerHTML=humidity;
+    descDisplay.innerHTML=conditions;
+    weatherEmoji.src=`https://raw.githubusercontent.com/visualcrossing/WeatherIcons/refs/heads/main/PNG/3rd%20Set%20-%20Color/${icon}.png`;
 }
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    weatherData();
+});
